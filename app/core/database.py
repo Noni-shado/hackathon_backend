cd1768c97f86ed0389699c045a87794fabc7f287
nouha <nohaelabrouki@gmail.com>
2025-12-12 12:21:24 +0100

    test

--- app/core/database.py
+++ app/core/database.py
@@ -1,3 +1,29 @@
+# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+
+# from app.core.config import settings
+
+# engine = create_async_engine(
+#     settings.DATABASE_URL,
+#     echo=True,
+#     future=True
+# )
+
+# AsyncSessionLocal = sessionmaker(
+#     engine, class_=AsyncSession, expire_on_commit=False
+# )
+
+# Base = declarative_base()
+
+
+# async def get_db():
+#     async with AsyncSessionLocal() as session:
+#         try:
+#             yield session
+#         finally:
+#             await session.close()
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -6,12 +32,15 @@ from app.core.config import settings
 
 engine = create_async_engine(
     settings.DATABASE_URL,
-    echo=True,
-    future=True
+    pool_pre_ping=True,   # évite connexions mortes
+    pool_recycle=300,     # recycle les connexions
+    echo=False            # IMPORTANT en prod / serverless
 )
 
 AsyncSessionLocal = sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
 )
 
 Base = declarative_base()
@@ -19,7 +48,4 @@ Base = declarative_base()
 
 async def get_db():
     async with AsyncSessionLocal() as session:
-        try:
-            yield session
-        finally:
-            await session.close()
+        yield session
